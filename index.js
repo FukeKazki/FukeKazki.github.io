6f24203a9cdc8474352bf23b94b7eda8b58e69e3
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-01-01 14:47:33 +0900

    background color setting
    to acoompany other setting

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 // JavaScript Document
 $(function(){
 //	ヘッダーの固定
-	if($('.nc').length){
+if($('.nc').length){
 	var gm_nav = $('.nc'), gm_offset = gm_nav.offset();
-		$(window).scroll(function () {
-		if($(window).scrollTop() > gm_offset.top) { gm_nav.addClass('fixed'); } else { gm_nav.removeClass('fixed'); }
+	$(window).scroll(function () {
+	if($(window).scrollTop() > gm_offset.top) { gm_nav.addClass('fixed'); } else { gm_nav.removeClass('fixed'); }
 	});
-	}
+}
 //	リンク移動
-	$('a[href^="#"]').click(function() {
-      var speed = 400;
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
-   });
+$('a[href^="#"]').click(function() {
+	var headerHight = 100;
+	var speed = 400;
+  	var href= $(this).attr("href");
+  	var target = $(href == "#" || href == "" ? 'html' : href);
+  	var position = target.offset().top-headerHight;
+  	$('body,html').animate({scrollTop:position}, speed, 'swing');
+  	return false;
+});
 
 });
