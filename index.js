60f449491ad2da9c3675833f606f19329ebb96fa
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-03-17 23:15:58 +0900

    🚚 : Rename variable

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // JavaScript Document
 $(function(){
+	'use strict';
 //	ヘッダーの固定
 if($('.nc').length){
-	var gm_nav = $('.nc'), gm_offset = gm_nav.offset();
+	let gm_nav = $('.nc');
+	let gm_offset = gm_nav.offset();
 	$(window).scroll(function () {
 	if($(window).scrollTop() > gm_offset.top) { gm_nav.addClass('fixed'); } else { gm_nav.removeClass('fixed'); }
 	});
 }
 //	リンク移動
 $('a[href^="#"]').click(function() {
-	var headerHight = 100;
-	var speed = 400;
-  	var href= $(this).attr("href");
-  	var target = $(href == "#" || href == "" ? 'html' : href);
-  	var position = target.offset().top-headerHight;
+	const headerHight = 100;
+	const speed = 400;
+  	const href= $(this).attr("href");
+  	const target = $(href == "#" || href == "" ? 'html' : href);
+  	const position = target.offset().top-headerHight;
   	$('body,html').animate({scrollTop:position}, speed, 'swing');
   	return false;
 });
@@ -33,4 +35,5 @@ $('a[href^="#"]').click(function() {
 		//collection: 'div',
 		image: 'com/img/sakura.png',
 	});
+	
 });
