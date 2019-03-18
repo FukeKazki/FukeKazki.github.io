64d6ee942cbfc0b118d5aae8cf9b04d6682be1dc
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-03-18 13:46:30 +0900

    Fix designs

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// JavaScript Document
+$(function(){
+	'use strict';
+	$('#header').load('../com/src/header.html');
+	$('#footer').load('../com/src/footer.html');
+	$('#gnav').load('../com/src/gnav.html', function() {
+		//	ヘッダーの固定
+		if($('.nc').length){
+			const gm_nav = $('.nc');
+			const gm_offset = gm_nav.offset();
+			$(window).scroll(function () {
+			if($(window).scrollTop() > gm_offset.top) { gm_nav.addClass('fixed'); } else { gm_nav.removeClass('fixed'); }
+			});
+		}
+		
+		$('a[href^="#"]').click(function() {
+			const headerHight = 100;
+			const speed = 400;
+			const href= $(this).attr("href");
+			const target = $(href == "#" || href == "" ? 'html' : href);
+			const position = target.offset().top-headerHight;
+			$('body,html').animate({scrollTop:position}, speed, 'swing');
+			return false;
+		});
+
+			$('#nav_toggle').click(function(){
+				$("header").toggleClass('open');
+				$('nav').slideToggle(500);
+			});
+	});
+	
+	//桜が降る
+//	$(document).snowfall({
+//		maxSize: 100,
+//		maxSpeed: 5,
+//		round: true,
+//		flakeColor: "pink",
+//		image: 'com/img/sakura.png',
+//	});
+	
+	
+});
